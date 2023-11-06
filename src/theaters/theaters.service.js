93cf5a33259a8832654843e3752a76d575bce513
Willie Parker <124847185+Willie3p3@users.noreply.github.com>
2023-11-05 20:09:18 -0500

    Update theaters.service.js

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,26 +1,18 @@
-const knex = require("../db/connection");
+const db = require("../db/connection");
 
 function list() {
-	return knex("theaters")
-		.select("*");
+  return db("theaters");
 }
 
-function listMovies(theaterId) {
-	return knex("movies_theaters as mt")
-		.join("movies as m", "m.movie_id", "mt.movie_id")
-		.where({ theater_id: theaterId })
-		.select("m.*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id");
-}
-
-function listTheaters(movieId) {
-	return knex("theaters as t")
-		.join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
-		.where({ movie_id: movieId })
-		.select("t.*", "mt.is_showing", "mt.movie_id");
+function getMovies(theaterId) {
+  return db("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "mt.movie_id", "m.movie_id")
+    .select("m.*", "mt.is_showing")
+    .where({ "t.theater_id": theaterId });
 }
 
 module.exports = {
-	list,
-	listMovies,
-	listTheaters,
-}
+  list,
+  getMovies,
+};
