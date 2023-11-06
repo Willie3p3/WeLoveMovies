77925a00ee9727b03dc41ffbeb8d95c2ba73515c
Willie Parker <124847185+Willie3p3@users.noreply.github.com>
2023-11-05 20:05:31 -0500

    Update reviews.controller.js

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,56 +1,54 @@
 const service = require("./reviews.service");
+const asyncErrorBoundary = require("../utils/errors/asyncErrorBoundary");
+
+async function _paramsCheck(req, res, next) {
+  const { reviewId } = req.params;
+  const match = await service.read(reviewId);
+  if (match.length === 0 || !reviewId)
+    return next({ status: 404, message: "Review cannot be found." });
+  res.locals.review = match[0];
+
+  next();
+}
+
+function _bodyCheck(req, res, next) {
+  const { data: { score = null, content = null } = {} } = req.body;
+  let updateObj = {};
+  if (!score && !content)
+    return next({ status: 400, message: "Missing score or content in body" });
+  if (score) updateObj.score = score;
+  if (content) updateObj.content = content;
+  res.locals.update = updateObj;
+  next();
+}
+
+async function list(req, res) {
+  const reviews = await service.list();
+  res.status(200).json({ data: reviews });
+}
+
+function read(req, res) {
+  res.status(200).json({ data: res.locals.review });
+}
+
+async function put(req, res) {
+  const { critic_id, review_id } = res.locals.review;
+  const update = res.locals.update;
+  await service.update(update, review_id);
+  const updatedReview = await service.read(review_id);
+  const critic = await service.getCritic(critic_id);
+  res.status(200).json({ data: { ...updatedReview[0], critic: critic[0] } });
+}
 
 async function destroy(req, res) {
-	await service.delete(Number(res.locals.review.review_id));
-	res.sendStatus(204);
-}
-
-async function validateReviewId(req, res, next) {
-	const { reviewId } = req.params;
-	const review = await service.read(Number(reviewId));
-
-	if(review) {
-		res.locals.review = review;
-		return next();
-	}
-
-	next({
-		status: 404,
-		message: "Review cannot be found."
-	});
-}
-
-async function update(req, res) {
-	const newReview = {
-		...req.body.data,
-		review_id: res.locals.review.review_id,
-	}
-
-	await service.update(newReview);
-	const review = await service.read(res.locals.review.review_id);
-
-	const reviewToReturn = {
-		...review,
-		critic: await service.readCritic(res.locals.review.critic_id),
-	}
-
-	res.json({ data: reviewToReturn });
-}
-
-async function readReviews(req, res) {
-	const reviews = await service.readReviews(res.locals.movie.movie_id);
-
-	for(let review of reviews) {
-		const critic = await service.readCritic(review.critic_id);
-
-		review["critic"] = critic;
-	}
-
-	res.json({ data: reviews });
+  const { review_id } = res.locals.review;
+  await service.destroy(review_id);
+  res.sendStatus(204);
 }
 
 module.exports = {
-	delete: [validateReviewId, destroy],
-	update: [validateReviewId, update],
-	readReviews,
+  list: [asyncErrorBoundary(list)],
+  read: [asyncErrorBoundary(_paramsCheck), read],
+  put: [asyncErrorBoundary(_paramsCheck), _bodyCheck, asyncErrorBoundary(put)],
+  delete: [asyncErrorBoundary(_paramsCheck), asyncErrorBoundary(destroy)],
 };
